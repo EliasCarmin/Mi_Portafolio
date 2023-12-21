feafcc4720949a3056a386c50a04237723f5a664
EliasCarmin <eliasjesuscarmin@gmail.com>
2023-12-21 12:16:29 -0500

    b

---
+++ FrontEnd/Portafolio/Dash1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMDJiY2M2ZDItNGUwOS00Y2NhLWIyMzktYmRmMDBiYTViMzQwIiwidCI6ImM0YTY2YzM0LTJiYjctNDUxZi04YmUxLWIyYzI2YTQzMDE1OCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMDJiY2M2ZDItNGUwOS00Y2NhLWIyMzktYmRmMDBiYTViMzQwIiwidCI6ImM0YTY2YzM0LTJiYjctNDUxZi04YmUxLWIyYzI2YTQzMDE1OCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+</body>
+</html>
