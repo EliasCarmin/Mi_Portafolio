adda6dafd9b1e6f42c84a833570d4815f0a5d7c5
EliasCarmin <eliasjesuscarmin@gmail.com>
2024-06-05 16:41:12 -0500

    index
    index spanish a index

---
+++ Index.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--ÍCONO DE PESTAÑA -->
+    <link rel="icon" href="/IMG/mishi blanco.png" type="image/x-icon">
+
+   <!--AOS-->
+   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">  
+    <!--librarys-->
+     <link rel="stylesheet" href="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     <!--CSS-->
+    <link rel="stylesheet" href="/CSS/index.css">
+
+    <title>Elías Jesús</title>
+</head>
+<body>
+
+
+    <!--Intro Animado-->
+    <div class="intro">
+        <img src="/IMG/elias-cn.png" alt="imagen-logo" width="30%" height="60%" class="imagen-logo">
+        <h1 class="logo">
+            <span class="logo-parts">Si</span>
+            <span class="logo-parts"> se</span>
+            <span class="logo-parts"> puede</span>
+            <span class="logo-parts"> imaginar,</span>
+            <span class="logo-parts"> se </span>
+            <span class="logo-parts"> puede</span>
+            <span class="logo-parts"> programar </span>
+        </h1>
+    </div>
+
+
+
+    <!--Encabezado-->
+    <header>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fas fa-bars"></i>
+        </label>
+        <img src="/IMG/prueba 1.jpeg" alt="" class="image_nav">
+              
+        <ul class="menu-principal">
+            <li><a href="#inicio">Inicio</a></li>
+            <li class="hola"><a href="">ACERCA DE MÍ</a>
+            <!--submenú-->
+            <ul class="sub-menu">
+                    <li><a href="#">Pasiones</a></li>
+                    <li><a href="#skill">Habilidades</a></li>
+                </ul>
+            <!--submenú-->
+            <li><a href="#experiencia">EXPERIENCIA</a></li>
+            <li><a href="#">CONTACTO</a></li>
+        </ul>
+    </nav>
+    </header>
+    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <!--Fin de encabezado-->
+
+    <!--CONTENIDO-->
+
+     <!--Sobre Mí-->
+     <div class="titulos_secciones" id="inicio">
+        <h1 class="">Sobre Mí</h1>
+    </div>
+    <div class="Sobremi">
+        <img src="/IMG/elias.jpg" alt="Tu Fotografía">
+        <div class="Sobremi-content">
+            <p>Buenas, soy Elías.<br> 
+            Soy ingeniero de Sistemas en Formación en los Últimos Ciclos, 
+            con Habilidad para trabajar en colaboración en equipos 
+            multidisciplinarios y pasión por el aprendizaje continuo.
+             Busco oportunidades para aplicar mis conocimientos en Ingeniería 
+             de Sistemas y adquirir experiencia laboral en un entorno desafiante, 
+             Mi pasíon por la tecnología está dirigido en el rubro de TI y Análisis de datos
+            </p>
+        </div>
+    </div>
+    
+    <!--Pasiones-->
+    <div class="titulos_secciones" id="pasion">
+        <h1 class="">Pasiones</h1>
+    </div>
+    
+    <div class="Pasions">
+        <div class="columns">
+            <div data-aos="fade-left">
+                <img src="/IMG/Black Simple Music Studio Logo.png" alt="" class="imagenes" >
+            </div>
+            
+            <h2 class="titulos">Música</h2>
+            <p>
+                Música: Hay muchas cosas para escapar de la realidad, entre ellas resalta la música; para mí ésta es un remedio y motor para cualquier momento,
+                 además en lo personal me gusta muchísimos géneros dispersos por todo el mundo.
+            </p>
+        </div>
+
+        <div class="columns">
+            <div data-aos="fade-left">
+            <img src="/IMG/Olivia Wilson Dance Club Logo.png" alt="" class="imagenes">
+            </div>
+            
+            <h2 class="titulos">Danza</h2>
+            <p>
+                Danza: “La fé me hace bailar”, bailar es una de mis grandes pasiones preferidos y practicados frecuentemente, 
+                en lo personal considero que las personas deben poseer un equilibrio mental y físico; soy consciente que mi carrera es una actividad muy sedentaria y por ello busco actividad física.
+            </p>
+        </div>
+
+        <div class="columns" >
+            <div data-aos="fade-left">
+            <img src="/IMG/Blue Minimal Lion Technology Free Logo.png" alt="" class="imagenes">
+            </div>
+            
+            <h2 class="titulos">Tecnología</h2>
+            <p>
+                Tecnología: La tecnología es mi pasión sin embargo la idea de que la ésta no es aprovechada es un poco descabellante, las herramientas y conocimientos en la palma de nuestra mano en toda disposición deben usarse en favor a la sociedad; 
+                el futuro está aquí entre nosotros en medio de éste mundo contemporáneo y digital.
+            </p>
+
+        </div>
+    </div>
+
+    <!--Portafolio-->
+    <div class="titulos_secciones" id="experiencia">
+        <h1 class="">Portafolio</h1>
+    </div>
+
+    <div class="experiencia">
+        <div class="item_portafolio">
+            <div class="img_experiencia">
+            <img src="/IMG/negro.webp" alt="">
+            </div>
+            <div class="item_descripcion">
+                <h2>Título</h2>
+                <p>blablablalbalbalblalbalblablalbablablablablabla
+                blablablalbalbalblalbalblablalbablablablablabla
+                blablablalbalbalblalbalblablalbablablablablabla
+                blablablalbalbalblalbalblablalbablablablablabla
+                </p>
+                <a href="">Ver más...</a>
+            </div>
+        </div>
+        <div class="item_portafolio">
+        <div class="img_experiencia">
+            <img src="/IMG/negro.webp" alt="">
+            </div>
+            <div class="item_descripcion">
+                <h2>Título</h2>
+            </div>
+        </div>
+        <div class="item_portafolio">
+        <div class="img_experiencia">
+            <img src="/IMG/negro.webp" alt="">
+            </div>
+            <div class="item_descripcion">
+              <h2>Título</h2>
+            </div>
+        </div>
+        <div class="item_portafolio">
+        <div class="img_experiencia">
+            <img src="/IMG/negro.webp" alt="">
+            </div>
+            <div class="item_descripcion">
+                <h2>Título</h2>
+            </div>
+        </div>
+        <div class="item_portafolio">
+        <div class="img_experiencia">
+            <img src="/IMG/negro.webp" alt="">
+            </div>
+            <div class="item_descripcion">
+               <h2>Título</h2>
+            </div>
+        </div>
+        <div class="item_portafolio">
+        <div class="img_experiencia">
+            <img src="/IMG/negro.webp" alt="">
+            </div>
+            <div class="item_descripcion">
+                <h2>Título</h2>
+            </div>
+        </div>
+    </div>
+  
+
+
+    <!--SKILL SETS-->
+    <div class="titulos_secciones">
+        <h1 class="">Skill Sets</h1>
+    </div>
+    
+    <div class="skill_set" id="skill">
+        <div class="container_skills">
+            
+            <!--LISTA DE HABILIDADES-->
+            <!--1-->
+            <div class="general-skill">
+                <!---->
+                <div class="box-imagen">
+                    <img src="/IMG/iconos.img/english.png" alt="" width: 500px;
+                    height: 50px;>
+                </div>
+    
+                <div class="skills">
+                    
+                    <div class="detalles">
+                            <span>Inglés</span>
+    
+                        <div class="sub-detalles2">
+                            <span id="number1">00</span> 
+                            <span>%</span>
+                        </div>
+                        
+                    </div>
+                    <div class="menu">
+                        <div id="ingles-menu"></div>
+                    </div>
+                </div>
+            </div>
+            <!--2-->
+            <div class="general-skill">
+
+            <div class="box-imagen">
+                <img src="/IMG/iconos.img/sql.png" alt="" width: 500px;
+                height: 50px;>
+            </div>
+            <div class="skills">
+                    <div class="detalles">
+                        <span>DataBase SQL y MySQL</span>
+                        <div class="sub-detalles2">
+                            <span class="number2">50</span>
+                            <span>%</span>
+                        </div>
+                        
+                    </div>
+                    <div class="menu">
+                        <div id="data-menu"></div>
+                    </div>
+            </div>
+        </div>
+            <!--3-->
+            <div class="general-skill">
+            <div class="box-imagen">
+                <img src="/IMG/iconos.img/python.png" alt="" width: 500px;
+                height: 50px;>
+            </div>
+             <div class="skills">
+                        <div class="detalles">
+                            <span><i></i> Python</span>
+                            <div class="sub_detalles2">
+                                <span class="number3">90</span>
+                                <span>%</span>
+                            </div>
+                            
+                        </div>
+                        <div class="menu">
+                         <div id="java-menu"></div>
+                        </div>
+            </div>
+        </div>
+          <!--4-->
+          <div class="general-skill">
+            <div class="box-imagen">
+                <img src="/IMG/iconos.img/java.png" alt="" width: 500px;
+                height: 50px;>
+            </div>
+             <div class="skills">
+                        <div class="detalles">
+                            <span><i></i> Java</span>
+                            <div class="sub_detalles2">
+                                <span class="number3">90</span>
+                                <span>%</span>
+                            </div>
+                            
+                        </div>
+                        <div class="menu">
+                         <div id="java-menu"></div>
+                        </div>
+            </div>
+        </div>
+         <!--5-->
+         <div class="general-skill">
+            <div class="box-imagen">
+                <img src="/IMG/iconos.img/powerbi.png" alt="" width: 500px;
+                height: 50px;>
+            </div>
+             <div class="skills">
+                        <div class="detalles">
+                            <span><i></i>Power Bi</span>
+                            <div class="sub_detalles2">
+                                <span class="number3">90</span>
+                                <span>%</span>
+                            </div>
+                            
+                        </div>
+                        <div class="menu">
+                         <div id="java-menu"></div>
+                        </div>
+            </div>
+        </div>
+    </div>
+</div>
+    <!--Pie de página-->
+    <footer>
+
+        <div class="container_footer">
+
+            <div class="box_footer">
+                <div class="logazo">
+                    <img src="/IMG/prueba 1.jpeg" alt="">
+                </div> 
+                <div class="texto_abajo">
+                    <p>Portafolio personal del estudiante de ingeniería de Sistemas Elías Carmín,
+                        actualmente cursando el octavo ciclo en la Universidad Tecnológica del
+                        Perú</p>
+                </div>
+            </div>
+
+            <div class="box_footer">
+                <h2>Mi Persona</h2>
+                <a href="#pasion">Pasiones</a>
+                <a href="#skill">Habilidades</a>
+                <a href="/FrontEnd/CV.php">CV Completo</a>
+            </div>
+            <div class="box_footer">
+                <h2>Contacto</h2>
+                <h3><i class="fa fa-phone-square"></i> eliasjesuscarmin@gmail.com</h3>
+                <h3><i class="fa fa-envelope"></i> +51 956 224 010</h3>
+            </div>
+            <div class="box_footer">
+                <h2>Redes Sociales</h2>
+                <a href="#"><i class="fab fa-facebook-square"></i> Facebook</a>
+                <a href="#"><i class="fab fa-twitter-square"></i> Gmail</a>
+                <a href="https://www.linkedin.com/in/el%C3%ADas-carm%C3%ADn-java/"><i class="fab fa-linkedin"></i> Linkedin</a>
+            </div>
+        </div>
+
+    </footer>
+
+
+    <!--JS-->
+
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+  </script>
+  <script src="/animacion.js/animated.js"></script>
+</body>
+</html>
