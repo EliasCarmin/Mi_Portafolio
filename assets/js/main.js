4d3fdd736bc9d1829e3360daf51c02ca59c90e1a
EliasCarmin <130704962+EliasCarmin@users.noreply.github.com>
2025-01-25 17:21:35 -0500

    cambios, hasta 1:08:50

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,13 +50,43 @@ const sendEmail = (e) =>{
 
         //clear input fields
         contactForm.reset()
+    }, () =>{
+        contactMessage.textContent = 'Mensaje de Error'
     })
 }
 
+contactForm.addEventListener('submit', sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
 
+    window.scrollY >= 350 
+        ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
