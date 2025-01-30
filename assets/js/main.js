adca9dd07db3cf76befcf05108f21c800c42de2d
EliasCarmin <130704962+EliasCarmin@users.noreply.github.com>
2025-01-30 16:42:48 -0500

    change of corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,3 +90,16 @@ window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal('.home__data, .expriencie, .skills')
+sr.reveal('.home__img'), {delay: 600}
+sr.reveal('.home__scroll'), {delay: 800}
+sr.reveal('.work__card, .services__card'), {interval: 100}
+sr.reveal('.about__content'), {interval: 100}
+sr.reveal('.about__img'), {interval: 100}
